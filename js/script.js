a8a7768f22ae31041d406b1e8e909a95d3603fc5
Irina Lukina <ibenderina@gmail.com>
2019-10-26 23:57:19 +0300

    Стилизация инпутов формы бронирования

--- js/script.js
+++ js/script.js
@@ -1,8 +1,33 @@
+function countMinus(n) {
+  if (parseInt(n.value) > 0) {
+    n.value = parseInt(n.value) - 1;
+  }
+}
+
+function countPlus(n) {
+    n.value = parseInt(n.value) + 1;
+}
+
+function initInputNumber(inputNumber) {
+  var inputMinus = inputNumber.querySelector(".booking__input-number-minus");
+  var inputPlus = inputNumber.querySelector(".booking__input-number-plus");
+  var numberCount = inputNumber.querySelector(".input-number-count");
+
+  inputMinus.addEventListener("click", function() {
+    countMinus(numberCount);
+  });
+  inputPlus.addEventListener("click", function() {
+    countPlus(numberCount);
+  });
+}
+
+var inputNumber = document.querySelectorAll(".booking__input-number");
+for (var i = 0; i < inputNumber.length; i++) {
+  initInputNumber(inputNumber[i]);
+}
+
 var buttonElement = document.querySelector(".booking__form-button");
-
 var formContain = document.querySelector(".booking__form-dates-choose");
-
 buttonElement.addEventListener("click", function(event) {
     formContain.classList.toggle("booking__form--active");
 })
-
